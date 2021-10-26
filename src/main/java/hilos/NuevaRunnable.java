e8585221f872c2da5a7036433c71f17240f3d1db
dordonezute <dordonez@ute.edu.ec>
2021-10-26 13:50:41 -0500

    Initial

---
+++ src/main/java/hilos/NuevaRunnable.java
@@ -0,0 +1,22 @@
+
+package hilos;
+
+/**
+ *
+ * @author dordonez@ute.edu.ec
+ */
+public class NuevaRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        for(int i = 0; i < 5; i++) {
+            System.out.println(name + " paso: " + i);
+            try {
+                Thread.sleep(700);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
